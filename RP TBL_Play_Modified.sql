6cee3970f8743a94107c7f97fda23109f292f8a2
rickscole <ricksaubercole@gmail.com>
2020-06-18 12:57:26 -0400

    Initial upload
    Schema for RP.TBL_Play_Modified. Not needed for implementation, since easiest method of uploading CSV is via SSMS Import Wizard, but helpful for validating schema datatypes

---
+++ RP TBL_Play_Modified.sql
@@ -0,0 +1,42 @@
+USE [NFL]
+GO
+
+/****** Object:  Table [RP].[TBL_Play_Modified]    Script Date: 6/18/2020 12:37:19 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [RP].[TBL_Play_Modified](
+	[id] [int] NULL,
+	[type] [int] NULL,
+	[q1] [int] NULL,
+	[q2] [int] NULL,
+	[q3] [int] NULL,
+	[q4] [int] NULL,
+	[b1_1] [int] NULL,
+	[b1_2] [int] NULL,
+	[b1_3] [int] NULL,
+	[q5] [int] NULL,
+	[b2_1] [int] NULL,
+	[b2_2] [int] NULL,
+	[b2_3] [int] NULL,
+	[b2_4] [int] NULL,
+	[b2_5] [int] NULL,
+	[b3_1] [int] NULL,
+	[b3_2] [int] NULL,
+	[b3_3] [int] NULL,
+	[b3_4] [int] NULL,
+	[q6] [int] NULL,
+	[b4_1] [int] NULL,
+	[q7] [int] NULL,
+	[q8] [int] NULL,
+	[q9] [int] NULL,
+	[q10] [int] NULL,
+	[q11] [int] NULL,
+	[PK_ID_Play] [int] NULL
+) ON [PRIMARY]
+GO
+
+
