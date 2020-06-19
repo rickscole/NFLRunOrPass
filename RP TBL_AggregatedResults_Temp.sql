a57daea78ff7b5f388faf8b8e4a736c0260014f3
rickscole <ricksaubercole@gmail.com>
2020-06-19 08:28:08 -0400

    Initial upload
    Staging table for aggregated results (across cross-validations) for a given forecast iteration

---
+++ RP TBL_AggregatedResults_Temp.sql
@@ -0,0 +1,15 @@
+USE [NFL]
+GO
+
+/****** Object:  Table [RP].[TBL_AggregatedResults_Temp]    Script Date: 11/5/2019 11:27:35 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [RP].[TBL_AggregatedResults_Temp](
+	[meanAccuracy] [float] NULL,
+	[stdevAccuracy] [float] NULL
+) ON [PRIMARY]
+GO
