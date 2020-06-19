7f2b683eb0a1cf0642ab6fd06209f8f4ae235dfa
rickscole <ricksaubercole@gmail.com>
2020-06-19 08:01:09 -0400

    Initial upload
    History table for Random Forest parameters across various iterations. See description for corresponding staging table

---
+++ RP TBL_ForecastParameters_RF.sql
@@ -0,0 +1,31 @@
+USE [NFL]
+GO
+
+/****** Object:  Table [RP].[TBL_ForecastParameters_RF]    Script Date: 1/31/2020 5:10:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [RP].TBL_ForecastParameters_RF(
+	[PK_ID_ForecastParameter_RF] [int] IDENTITY(1,1) NOT NULL,
+	[FK_ID_forecastIteration] [int] NULL,
+	[tempoTimbri_SVMforecastParameter] [datetime2](7) NULL,
+	[estimators] [nvarchar](1024) NULL,
+	[splitCriterion] [nvarchar](1024) NULL,
+	[maxDepth] [nvarchar](1024) NULL,
+	[minSampleSplit] [nvarchar](1024) NULL,
+	[minSampleLeaf] [nvarchar](1024) NULL,
+	[maxFeatures] [nvarchar](1024) NULL,
+	[maxLeafNodes] [nvarchar](1024) NULL,
+	[minImpurityDecrease] [nvarchar](1024) NULL,
+	[bootstrap] [nvarchar](1024) NULL,
+	[OOBScore] [nvarchar](1024) NULL,
+	[usesPCA] [nvarchar](1024) NULL,
+	[numberOfComponents] [nvarchar](1024) NULL,
+	[standardizeData] [nvarchar](1024) NULL,
+	[numberOfSplits] [nvarchar](1024) NULL,
+	[evalMetric] [nvarchar](1024) NULL
+) ON [PRIMARY]
+GO
